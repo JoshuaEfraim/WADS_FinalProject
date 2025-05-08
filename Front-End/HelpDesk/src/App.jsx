7532467e3a2b017a658896fd9fbc279c457719f5
Suwandi The <144767691+Suwandithe@users.noreply.github.com>
2025-05-08 12:47:45 +0700

    frontend setup2

---
+++ Front-End/HelpDesk/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+    <h1 className="text-primary-500 font-bold text-2xl" >Hello world</h1>
+  )
+}
+
+export default App
--- frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return(
-    <h1 className='font-bold text-primary-500'>Hello world</h1>
-  )
-}
-
-export default App
