6628a0c8e959db156fbd15c778323d277f5c7605
Suwandi The <144767691+Suwandithe@users.noreply.github.com>
2025-06-02 20:42:53 +0700

    redeploy

--- backend/server.js
+++ backend/server.js
@@ -24,16 +24,16 @@ app.use(cors({
 // Add cookie parser middleware
 app.use(cookieParser());
 
-// Add session middleware
-// app.use(session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       secure: process.env.NODE_ENV === 'production',
-//       maxAge: 24 * 60 * 60 * 1000 // 1 day
-//     }
-// }));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 24 * 60 * 60 * 1000 // 1 day
+    }
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
