daede00e957a65ce35c326a5cdcfe875b60371d4
Suwandi The <144767691+Suwandithe@users.noreply.github.com>
2025-06-02 23:40:22 +0700

    redeploy

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/service/user/auth/google/callback',
+      callbackURL: '/api/user/auth/google/callback',
 
     },
     async (accessToken, refreshToken, profile, done) => {
